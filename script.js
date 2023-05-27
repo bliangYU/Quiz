faebd1da27c32c9ab6ebe6784fb8f46740b76ca2
bliangYU <128714733+bliangYU@users.noreply.github.com>
2023-05-26 23:43:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,191 @@
+(function(){
+  // Functions
+  function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+
+        // variable to store the list of possible answers
+        const answers = [];
+
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
+
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults(){
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+    myTimer();
+
+    const card = document.querySelector('.card');
+    card.classList.add('flipped');
+
+    setTimeout(() => {
+      card.classList.remove('flipped');
+    }, 500);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  // Variables
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "How many Infinity Stones are there?",
+      answers: {
+        a: "3",
+        b: "5",
+        c: "6",
+        d: "10"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What’s the heaviest organ in the body?",
+      answers: {
+        a: "Brain",
+        b: "Liver",
+        c: "Skin",
+        d: "Heart"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What is the main ingredient in a falafel?",
+      answers: {
+        a: "Lentil",
+        b: "Chickpea",
+        c: "Broccoli",
+        d: "Split pea"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What country is Shakira from?",
+      answers: {
+        a: "Mexico",
+        b: "Puerto Rico",
+        c: "Paraguay",
+        d: "Colombia"
+      },
+      correctAnswer: "d"
+    }
+  ];
+
+  // Kick things off
+  buildQuiz();
+
+  // Pagination
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  // Show the first slide
+  showSlide(currentSlide);
+
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+
+
+  var sec = 10;
+  var time = setInterval(myTimer, 1000);
+
+function myTimer() {
+  document.getElementById('timer').innerHTML = sec;
+  sec--;
+
+  if (sec === -1) {
+    clearInterval(time);
+    showNextSlide(); // Go to the next question
+    sec = 10; // Reset the timer to 5 seconds
+    time = setInterval(myTimer, 1000); // Start the timer again
+  }
+}
+})();
